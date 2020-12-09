82b5d30ff55ec88fccdfcc5dc260af69bf495b3e
echo094 <20028238+echo094@users.noreply.github.com>
2020-12-09 21:05:05 +0800

    Use Element Plus and update home page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,61 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <el-menu
+    class="el-menu-demo"
+    mode="horizontal"
+    router>
+    <el-menu-item index="/">
+      <i class="el-icon-s-home"></i>
+      <span>首页</span>
+    </el-menu-item>
+  </el-menu>
   <router-view/>
+  <el-dialog
+    title="提示"
+    v-model="isInvalidBrowser"
+    fullscreen
+    :show-close="false">
+    <p>请使用其它浏览器打开</p>
+    <p>当前浏览器 {{ currentBrowser }}</p>
+  </el-dialog>
 </template>
 
+<script>
+export default {
+  name: 'app',
+  mounted () {
+    if (!this.checkBrowser()) {
+      this.isInvalidBrowser = true
+    }
+  },
+  data () {
+    return {
+      isInvalidBrowser: false,
+      currentBrowser: ''
+    }
+  },
+  methods: {
+    checkBrowser () {
+      const str = navigator.userAgent
+      if (~str.indexOf('MicroMessenger')) {
+        this.currentBrowser = 'Weixin'
+        return false
+      }
+      return true
+    }
+  }
+}
+</script>
+
 <style>
+body {
+  background: #F8F8F8;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,84 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1 style="text-align: center;">Welcome</h1>
+    <el-card shadow="always">
+      <div style="text-align: center;">
+        <strong>系统信息</strong>
+      </div>
+      <el-table
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column
+          prop="name"
+          label="属性"
+          width="100">
+        </el-table-column>
+        <el-table-column
+          prop="data"
+          label="值">
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+/**
+ * 获取DPI
+ */
+function jsGetDPI () {
+  const arrDPI = []
+  if (window.screen.deviceXDPI !== undefined) {
+    arrDPI[0] = window.screen.deviceXDPI
+    arrDPI[1] = window.screen.deviceYDPI
+  } else {
+    var tmpNode = document.createElement('DIV')
+    tmpNode.style.cssText = 'width:1in;height:1in;position:absolute;left:0px;top:0px;z-index:99;visibility:hidden'
+    document.body.appendChild(tmpNode)
+    arrDPI[0] = parseInt(tmpNode.offsetWidth)
+    arrDPI[1] = parseInt(tmpNode.offsetHeight)
+    tmpNode.parentNode.removeChild(tmpNode)
+  }
+  return arrDPI
+}
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      activeName: 'first',
+      tableData: [
+        {
+          name: 'Platform',
+          data: ''
+        }, {
+          name: 'Language',
+          data: ''
+        }, {
+          name: 'Screen',
+          data: ''
+        }, {
+          name: 'DPI',
+          data: ''
+        }, {
+          name: 'Name',
+          data: ''
+        }, {
+          name: 'UserAgent',
+          data: ''
+        }
+      ]
+    }
+  },
+  mounted () {
+    const dpi = jsGetDPI()
+    this.tableData[0].data = navigator.platform
+    this.tableData[1].data = navigator.language
+    this.tableData[2].data = `(${window.screen.width}, ${window.screen.height}, ${window.devicePixelRatio})`
+    this.tableData[3].data = `(${dpi[0]}, ${dpi[1]})`
+    this.tableData[4].data = navigator.appName
+    this.tableData[5].data = navigator.userAgent
   }
 }
 </script>
